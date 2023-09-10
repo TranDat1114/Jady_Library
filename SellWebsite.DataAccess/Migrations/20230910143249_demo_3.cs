78478ba535907ab4df54a220d03d3bf25ac8e687
trandat1114 <cctalk1114@gmail.com>
2023-09-11 02:26:20 +0700

    ok

---
+++ SellWebsite.DataAccess/Migrations/20230910143249_demo_3.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SellWebsite.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class demo_3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsReturn",
+                table: "OrderHeaders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsReturn",
+                table: "OrderHeaders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
